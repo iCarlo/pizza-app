7e12e1b4f85758169bd15200b2f2689f9eae42cf
iCarlo <jcbbasilio28@gmail.com>
2026-02-10 14:58:22 +0800

    server setup and sample data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,22 @@ const App = () => {
       <Pizza
         name="The Chicken Texas BBQ Pizza"
         description="Chicken, BBQ sauce, red onions, and cilantro on a crispy crust."
+        image="/public/pizzas/bbq_ckn.webp"
       />
       <Pizza
         name="The Margherita Pizza"
         description="Fresh mozzarella, tomatoes, and basil on a thin crust."
+        image="/public/pizzas/mexicana.webp"
       />
       <Pizza
         name="The Pepperoni Pizza"
         description="Pepperoni, mozzarella cheese, and tomato sauce on a thick crust."
+        image="/public/pizzas/pepperoni.webp"
       />
       <Pizza
         name="The Veggie Supreme Pizza"
         description="Bell peppers, onions, mushrooms, olives, and tomatoes on a whole wheat crust."
+        image="/public/pizzas/veggie_veg.webp"
       />
     </div>
   );
