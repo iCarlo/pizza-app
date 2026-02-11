28b77e3b3ba9e68618896a60c82332de329fcf55
iCarlo <jcbbasilio28@gmail.com>
2026-02-11 18:30:00 +0800

    added cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { createRoot } from "react-dom/client";
 import Order from "./Order";
 import PizzaOfTheDay from "./PizzaOfTheDay";
+import Header from "./Header";
+import { CartContext } from "./contexts";
+import { useState } from "react";
 
 const App = () => {
+  const cartHook = useState([]);
+
   return (
-    <div>
-      <h1 className="logo">El Carlo</h1>
-      <Order />
-      <PizzaOfTheDay />
-    </div>
+    <CartContext.Provider value={cartHook}>
+      <div>
+        <Header />
+        <Order />
+        <PizzaOfTheDay />
+      </div>
+    </CartContext.Provider>
   );
 };
 
