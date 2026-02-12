a173c34553c49bb41514ea01d3a1727daeec3326
iCarlo <jcbbasilio28@gmail.com>
2026-02-12 11:34:41 +0800

    added past orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 const container = document.getElementById("root");
