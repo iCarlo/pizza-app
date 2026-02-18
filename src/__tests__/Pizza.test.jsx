2a98044cead6016ecff3d7864924043ee8b8f432
iCarlo <jcbbasilio28@gmail.com>
2026-02-18 09:31:50 +0800

    unit test on Pizza component

---
+++ src/__tests__/Pizza.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import { expect, test } from "vitest";
+import Pizza from "../Pizza";
+
+test("alt text renders on Pizza image", async () => {
+  const name = "My Fav Pizza";
+  const src = "https://picsum.photos/200";
+
+  const screen = render(
+    <Pizza name={name} image={src} description="A delicious pizza" />,
+  );
+
+  const img = screen.getByRole("img");
+  expect(img.src).toBe(src);
+  expect(img.alt).toBe(name);
+});
